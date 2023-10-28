50f865cfe299d5eb646b4ae73ee500198e53b58b
ch-epfl-376779 <ch-epfl-376779@noto.epfl.ch>
2023-10-29 00:58:51 +0200

    checked all

--- implementations.py
+++ implementations.py
@@ -1,17 +1,6 @@
 import numpy as np
 
 
-def compute_gradient_mse(y, tx, w):
-    """
-    Compute the gradient for mean squared error.
-    """
-    y = y.reshape(-1, 1)
-    w = w.reshape(-1, 1)
-    e = y - tx @ w
-    gradient = -tx.T @ e / len(y)
-    return gradient
-
-
 def sigmoid(t):
     """apply sigmoid function on t.
 
@@ -24,14 +13,12 @@ def sigmoid(t):
     return 1.0 / (1 + np.exp(-t))
 
 
-def compute_gradient_logistic(y, tx, w):
+def compute_gradient_mse(y, tx, w):
     """
-    Compute the gradient for logistic regression.
+    Compute the gradient for mean squared error.
     """
-    y = y.reshape(-1, 1)
-    w = w.reshape(-1, 1)
-    prediction = sigmoid(tx @ w)
-    gradient = tx.T @ (prediction - y) / len(y)
+    e = y - tx @ w
+    gradient = -tx.T @ e / len(y)
     return gradient
 
 
@@ -54,30 +41,31 @@ def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
     w = initial_w
     for _ in range(max_iters):
         i = np.random.randint(0, len(y))
-        gradient = compute_gradient_mse(y[i], tx[i], w)
+        gradient = compute_gradient_mse(y[i : i + 1], tx[i : i + 1], w)
         w = w - gamma * gradient
     loss = 0.5 * np.mean((y - tx @ w) ** 2)
     return w, loss
+
+
+def compute_gradient_logistic(y, tx, w):
+    """
+    Compute the gradient for logistic regression.
+    """
+    prediction = sigmoid(tx @ w)
+    gradient = tx.T @ (prediction - y) / len(y)
+    return gradient
 
 
 def logistic_regression(y, tx, initial_w, max_iters, gamma):
     """
     calculate logistic regression loss
     """
-    # compute the predicted probabilities
-    pred = 1.0 / (1.0 + np.exp(-tx.dot(w)))
-
-    # compute the logistic regression loss
-    loss = -np.sum(y * np.log(pred) + (1 - y) * np.log(1 - pred))
-
-    # compute the gradient of the loss
-    gradient = tx.T.dot(pred - y)
-
-    # compute the hessian matrix of the loss
-    S = np.diag((pred * (1 - pred)).ravel())
-    hessian = tx.T.dot(S).dot(tx)
-
-    return loss, gradient
+    w = initial_w
+    for _ in range(max_iters):
+        gradient = compute_gradient_logistic(y, tx, w)
+        w = w - gamma * gradient
+    loss = np.mean(y * np.log(sigmoid(tx @ w)) + (1 - y) * np.log(1 - sigmoid(tx @ w)))
+    return w, -loss
 
 
 def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
