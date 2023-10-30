b4742146b6b2cbeef90c21e54533b5c4fe6be45c
DoniMoon <mhd0712@gmail.com>
2023-10-30 18:17:52 +0100

    apply black

--- run.py
+++ run.py
@@ -2,15 +2,19 @@ from helpers import *
 import numpy as np
 from implementations import *
 
-#Load the data
+# Load the data
 datapath = "dataset/"
 x_train, x_test, y_train, train_ids, test_ids = load_csv_data(datapath)
 
-#Clean and Standardize the data
+# Clean and Standardize the data
 x_train_stripped, x_test_stripped = remove_empty_columns(x_train, x_test, 0.6)
-x_train_standardized, x_test_standardized = standardize_data(x_train_stripped, x_test_stripped)
+x_train_standardized, x_test_standardized = standardize_data(
+    x_train_stripped, x_test_stripped
+)
 
-w, loss = mean_absolute_error_gd(y_train, x_train_standardized, np.zeros(x_train_standardized.shape[1]), 500, 0.01)
+w, loss = mean_absolute_error_gd(
+    y_train, x_train_standardized, np.zeros(x_train_standardized.shape[1]), 500, 0.01
+)
 
 result = x_test_standardized.dot(w)
 binary_predictions = np.where(result >= 0.9, 1, -1)
